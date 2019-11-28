2f7c50875972f64c58dd461e60a86ef60fea80f4
steve.yang <yangguodang@foxmail.com>
2019-11-28 12:33:04 +0800

    commit: 141. Linked List Cycle

---
+++ HasCycle.cpp
@@ -0,0 +1,25 @@
+#define NULL nullptr
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == NULL || head->next == NULL)
+            return false;
+        ListNode *first = head;
+        ListNode *second = head->next;
+        while (second != NULL && second->next != NULL)
+        {
+            if (first == second)
+                return true;
+            first = first->next;
+            second = second->next->next;
+        }
+        return false;
+    }
+};
