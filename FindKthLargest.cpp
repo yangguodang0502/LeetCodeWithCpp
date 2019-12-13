d1d812773a863931a788d2d7e9d6f2221f99801b
steve.yang <yangguodang@foxmail.com>
2019-12-13 13:31:14 +0800

    commit: 215. Kth Largest Element in an Array

---
+++ FindKthLargest.cpp
@@ -0,0 +1,18 @@
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> q;
+        for (auto it:nums)
+        {
+            q.push(it);
+            if (q.size() > k)
+                q.pop();
+        }
+        return q.top();
+    }
+};
