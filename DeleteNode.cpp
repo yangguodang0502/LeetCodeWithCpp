7ee2a6912919ee0fa07be407e497df67f512a706
steve.yang <yangguodang@foxmail.com>
2019-12-20 13:29:13 +0800

    commit: 237. Delete Node in a Linked List

---
+++ DeleteNode.cpp
@@ -0,0 +1,15 @@
+#define NULL nullptr
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
